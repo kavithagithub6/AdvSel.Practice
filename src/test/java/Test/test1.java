129a44f25a4771514fad2919daa59f94511fb276
kavitha.r <kavitha.r@Kavitha>
2023-11-16 11:59:42 +0530

    test branch

--- src/test/java/Test/test1.java
+++ src/test/java/Test/test1.java
@@ -5,8 +5,9 @@ public class test1 {
 	public static void main(String[] args) {
 
 		System.out.println("Good Morning Test");
-		System.out.println("New change");
-
+		System.out.println("New change"); 
+		
+		System.out.println("Branch creation");
 	}
 
 }
